d5441a94cd1ad8dfa829eaf60a7637118fd7e57a
temaxo <melnikov.artem294@gmail.com>
2025-11-28 11:35:06 +0300

    [feat] Updated style's margins; created link on GitHub

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ class Application {
             saveUninitialized: false,
             cookie: {
                 secure: false,
-                maxAge: 24 * 60 * 60 * 1000
+                maxAge: 30 * 24 * 60 * 60 * 1000
             }
         }));
 
